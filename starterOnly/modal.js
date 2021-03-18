aab48cc94ca4ba9a3056f66352dbe0601bd2e779
Priscille LR <priscille.louisrose@gmail.com>
2021-03-18 19:54:58 +0100

    first name validation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -20,4 +20,27 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+//validate value input
 
+const validation = document.getElementById('btn-submit');
+const firstname = document.getElementById('first');
+const firstname_missing = document.getElementById('firstname_missing');
+const firstname_validation = /^[a-zA-Z '-]{2,}$/;
+
+validation.addEventListener('click', validate);
+
+function validate(e) {
+if (firstname_validation.test(firstname.value) == false) { //incorrect input -> prevent form sending
+    e.preventDefault();
+    showError();
+  } 
+}
+
+function showError() {
+  if (firstname.value == 0) {
+    firstname_missing.innerHTML = 'Prénom manquant';
+  } else {
+    firstname_missing.innerHTML = 'Format incorrect';
+  }
+  firstname_missing.style.color = 'red';
+}
